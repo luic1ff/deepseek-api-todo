cef205eed1c73270ef18314e9a8c728004244dd6
Smakouz Illea <i.smakouz@roxydesk.com>
2025-09-06 23:19:03 +0300

    init commit

--- frontend/src/app/middleware/GuestMiddleware.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import type {JSX} from "react";
-
-export const GuestMiddleware = (element: JSX.Element) => {
-    const isLoggedIn = false;
-    if (!isLoggedIn) return <Navigate to="/login" replace />;
-    return element;
-};
--- frontend/src/shared/types/api.ts
+++
@@ -1,5 +0,0 @@
-export interface ApiResponse<T = any> {
-    status: 'success' | 'error' | 'warning';
-    message: string;
-    data: T;
-}
